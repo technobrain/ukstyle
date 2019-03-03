dce4d2dafcfa9c910543eddefd7c8c31e0d70666
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2019-03-04 08:33:37 +0900

    Define templates path

--- lib/ukstyle/engine.rb
+++ lib/ukstyle/engine.rb
@@ -1,5 +1,10 @@
 module Ukstyle
   class Engine < ::Rails::Engine
+    # Configure template path
+    require 'byebug'
 
+    config.generators.templates.unshift(
+      File.expand_path('templates', __dir__)
+    )
   end
 end
