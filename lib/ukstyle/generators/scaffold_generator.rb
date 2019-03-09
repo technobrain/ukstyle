6d3283798abc0c4e8d6d0e0d61f1fa372f105dd5
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2019-03-10 00:07:14 +0900

    Support scaffold generator with theme

--- lib/ukstyle/generators/scaffold_generator.rb
+++ lib/ukstyle/generators/scaffold_generator.rb
@@ -9,8 +9,22 @@ module Ukstyle
       desc "このジェネレータはlib/templates/your_themeのテンプレートを使用してScaffoldGeneratorを実行します"
 
       
+      class_option :theme
+
+      def initialize(*args)
+        options = args.extract_optins!
+        return unless optins[:theme]
+        Rails::Generators.template_path.unshift(theme_path)
+      end
+
       hook_for :scaffold
       # hook_for :scaffold_controller, required: true
+
+      private
+
+      def theme_path
+        Rails.root.join('lib', 'templates', options[:theme])
+      end
     end
   end
 end
