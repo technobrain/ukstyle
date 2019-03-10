9bfbbdee77c56619352b421664d98484c0da80d4
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2019-03-10 13:09:29 +0900

    Fix dummy app

--- test/dummy/config/initializers/ukstyle.rb
+++ test/dummy/config/initializers/ukstyle.rb
@@ -15,9 +15,9 @@
 #   #                  helper_specs: false
 
 #   # g.test_framework :minitest, spec: false, fixture: true
-#   # g.system_tests nil
+#   g.system_tests nil
 #   # g.fallbacks[:minitest] = :rspec
-#   # g.templates.unshift File::expand_path('../../lib/templatesx', __FILE__)
+#   # g.templates.unshift File::expand_path('../../../lib/templates', __FILE__)
 # end
 
 # UikitAdminRails.configure do |config|
