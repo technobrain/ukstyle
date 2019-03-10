7506e7c01d0f80103878f550e9b3fbf7d5572987
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2019-03-11 00:04:12 +0900

    Remove unused code

--- lib/ukstyle/generators/scaffold/scaffold_generator.rb
+++ lib/ukstyle/generators/scaffold/scaffold_generator.rb
@@ -6,8 +6,6 @@ require 'byebug'
 
 module Ukstyle
   module Generators
-    # class ScaffoldGenerator < Rails::Generators::ScaffoldGenerator
-    # class ScaffoldGenerator < Rails::Generators::ResourceGenerator # :nodoc:
     class ScaffoldGenerator < Rails::Generators::NamedBase # :nodoc:
 
       desc "このジェネレータはlib/templates/your_themeのテンプレートを使用してScaffoldGeneratorを実行します"
@@ -30,40 +28,6 @@ module Ukstyle
         require "rails/generators/rails/scaffold/scaffold_generator"
         Rails::Generators::ScaffoldGenerator.start
       end
-
-# debugger
-      # hook_for :scaffold # , in: :rails
-      # hook_for :resource, in: :rails
-
-      # debugger
-      # hook_for :scaffold_controller, required: true, as: :rails, as: :scaffold
-
-      # remove_hook_for :resource_controller
-      # remove_class_option :actions
-
-      # class_option :api, type: :boolean
-      # class_option :stylesheets, type: :boolean, desc: "Generate Stylesheets"
-      # class_option :stylesheet_engine, desc: "Engine for Stylesheets"
-      # class_option :assets, type: :boolean
-      # class_option :resource_route, type: :boolean
-      # class_option :scaffold_stylesheet, type: :boolean
-
-      # def handle_skip
-      #   @options = @options.merge(stylesheets: false) unless options[:assets]
-      #   @options = @options.merge(stylesheet_engine: false) unless options[:stylesheets] && options[:scaffold_stylesheet]
-      # end
-
-      # hook_for :scaffold_controller, in: :rails, as: :scaffold_controller, required: true
-
-      # hook_for :assets, in: :rails, as: :assets do |assets|
-      #   invoke assets, [controller_name]
-      # end
-
-      # # hook_for :stylesheet_engine do |stylesheet_engine|
-      # #   if behavior == :invoke
-      # #     invoke stylesheet_engine, [controller_name]
-      # #   end
-      # # end
 
       private
 
