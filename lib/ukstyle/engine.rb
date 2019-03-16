f9196f29b786b328c43c6f3b6abf62d2c633a825
KOBAYASHI-Toshinobu <toshinobu_kobayashi@tbn.co.jp>
2019-03-16 11:09:18 +0900

    Remove unused code

--- lib/ukstyle/engine.rb
+++ lib/ukstyle/engine.rb
@@ -2,8 +2,6 @@ require 'byebug'
 
 module Ukstyle
   class Engine < ::Rails::Engine
-
-    # OK
     Rails::Generators.options.merge!(ukstyle: { orm: :active_record })
   end
 end
--- lib/ukstyle/generators/model/model_generator.rb
+++ lib/ukstyle/generators/model/model_generator.rb
@@ -8,8 +8,6 @@ module Ukstyle
       include Rails::Generators::ModelHelpers
       argument :attributes, type: :array, default: [], banner: "field[:type][:index] field[:type][:index]"
 
-      # class_option :orm, banner: "NAME", type: :string, default: :active_record, # required: true,
-      #                    desc: "ORM to generate the model"
       hook_for :orm, required: true, desc: "ORM to be invoked"
     end
   end
